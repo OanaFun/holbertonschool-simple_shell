413253e8d5ea9c10847e8c34908311c446170fec
Storm2601 <kevinguedon2601@gmail.com>
2023-12-13 16:35:19 +0100

    add missing libraries and prototypes

--- main.h
+++ main.h
@@ -1,10 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
+/** Librairies */
+#include <string.h> /** manipulate strings */
+#include <stdio.h>  /** manage inputs/outputs */
+#include <stdlib.h> /** memory management, random number string */
+                    /** conversions, etc */
+#include <unistd.h> /** access to the POSIX operating system API */
+#include <stdarg.h> /** accept a variable number of arguments */
+#include <sys/stat.h>   /** define types and functions to manipulate */
+                        /** file and directory information */
+#include <sys/wait.h> /** waits for the end of a child process */
+#include <sys/types.h> /** contains definitions of data types used by /**
+                        /** system functions 
+/** prototypes */
+int main(void);
+char *get_env(char *name);
+char *find_path(char *str, char *input);
+char *attach_path(char *str, char **input);
+char **tokenize_buffer(char *buffer);
+void shell_exit(char **token_array, char *buffer, int status);
+int fork_handler(char **token_array, char *buffer, int status);
+int execute(char **token_array, char *buffer, int count, int status);
+void print_env(void);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+void free_grid(char **grid);
+char *_strstr(char *haystack, char *needle);
 
 #endif
