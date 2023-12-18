1187f88e60e8d9969ef1b0f811343a9bbb9cd517
OanaFun <7491@holbertonstudents.com>
2023-12-18 06:39:14 -0800

    Add check_path file

---
+++ check_path.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * check_path - check if command is in the path directories
+ * @string: an array of strings
+ * @env: an array of environment variables
+ * Return: pointer to the linked list
+*/
+void check_path(char **string, char **env)
+{
+	char *str1, *str2, *path;
+	int i = 0, start = 0, last = 0;
+
+	str1 = _strcat("/", string[0]);
+	path = _getenv("PATH", env);
+	if (path == NULL)
+	{
+		free(str1);
+		exit(0);
+	}
+	while (path[i])
+	{
+		if (path[i] == ':' || path[i + 1] == '\0')
+		{
+			if (path[i + 1] == '\0')
+			{
+				i++;
+				last = 1;
+			}
+			else
+			{
+				path[i] = '\0';
+			}
+			str2 = _strcat(path + start, str1);
+			if (access(str2, F_OK) == 0)
+			{
+				free(string[0]);
+				string[0] = str2;
+				free(str1);
+				return;
+			}
+			free(str2);
+			if (last)
+			{
+				break;
+			}
+			path[i] = ':';
+			start = i + 1;
+		}
+		i++;
+	}
+	free(str1);
+}
