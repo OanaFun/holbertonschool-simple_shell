26d213d57e810d0ce19c8be3f6e70ba6c7ac956c
OanaFun <7491@holbertonstudents.com>
2023-12-20 16:20:23 -0800

    Modify function prototypes

--- main.h
+++ main.h
@@ -13,17 +13,12 @@
 #include <sys/wait.h> /** waits for the end of a child process */
 #include <sys/types.h> /** contains definitions of data types used by */
                         /** system functions */
-/** prototypes */
-int main(void);
-int _strlen(char *s);
-int _strcmp(char *s1, char *s2);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, char *src);
-char *_strdup(char *str);
-char *_strstr(char *haystack, char *needle);
-char *_strtok(char *str, const char *delim);
-char *_getenv(const char *name, char **env);
-void check_path(char **string, char **env);
+/** Prototypes */
+char *_getenv(const char *name, char **env)
+void _printenv(void)
+int empty_line(char *buff)
+int execute(char **args)
+char **_split(char *str, const char *delim)
+char *search_path(char *command)
 
 #endif
